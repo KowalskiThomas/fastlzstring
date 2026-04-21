00105e9345abe9d50bcb7157493b26c777faec16
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2026-04-21 08:32:03 +0200

    feat: add fast pylzstring implementation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("lzstr.BitString", ["src/BitString.pyx"]),
+    Extension("lzstr.Exceptions", ["src/Exceptions.pyx"]),
+    Extension("lzstr.LZString", ["src/LZString.pyx"]),
+]
+
+setup(
+    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+)
